02d516f589c046f6045d872e74c25e958e457ce4
prabha-2 <prabhagurunaathan@gmail.com>
2026-06-30 17:36:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello from Flask! 🚀 , I Achieved it, My First Milestone !!!"
+
+
+@app.route('/health')
+def health():
+    return "OK"
+
+@app.route('/test')
+def test():
+    return "It looks cool!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
